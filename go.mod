14f007ae0dcb311f00c37a49354441a107f09621
Carlos Lespin <carlos.lespin.silva@gmail.com>
2025-01-05 18:12:49 -0400

    feat: init

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.23.2
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	golang.org/x/crypto v0.31.0
+)
+
+require (
+	github.com/a-h/templ v0.3.819 // indirect
 	github.com/mattn/go-sqlite3 v1.14.24 // indirect
 )
--- main.go
+++ main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"database/sql"
-	"html/template"
-	"journal-lite/internal/database"
-	"log"
 	"net/http"
-
-	"github.com/mattn/go-sqlite3"
 )
 
+type Entry struct {
+	Id          int    `json:"id"`
+	Content     string `json:"content"`
+	DateCreated string `json:"date_created"`
+}
+
 func main() {
-	err := database.Initialize()
-	if err != nil {
-		log.Fatalf("Database initialization failed: %v", err)
-	}
-
 	http.HandleFunc("/", HomePage)
-	http.HandleFunc("/load", LoadContent)
-
 	http.ListenAndServe(":8080", nil)
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
-	tmpl.Execute(w, nil)
-}
-
-func LoadContent(w http.ResponseWriter, r *http.Request) {
-	html := `<div class="p-4 bg-blue-100">
-               <p>Content loaded via HTMX!</p>
-             </div>`
-	w.Write([]byte(html))
+	w.Write([]byte("hello"))
 }
